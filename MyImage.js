f046f73ebbdb553bae9ebebf5e50a9fb487feea3
Barbara Chacin <74036943+barbiechm@users.noreply.github.com>
2025-03-22 23:04:49 -0400

    Add files via upload

--- MyImage.js
+++ MyImage.js
@@ -1,25 +1,24 @@
-// Mostrar/ocultar capas ISO/OSI y desplazarse hacia ellas
+
 document.getElementById('isoButton').addEventListener('click', function () {
   const isoLayers = document.getElementById('isoLayers');
 
-  // Mostrar u ocultar las capas ISO/OSI
   if (isoLayers.classList.contains('visible')) {
     isoLayers.classList.remove('visible');
     setTimeout(() => {
       isoLayers.classList.add('hidden');
-    }, 500); // Espera a que termine la animación
+    }, 500); 
   } else {
     isoLayers.classList.remove('hidden');
     setTimeout(() => {
       isoLayers.classList.add('visible');
-    }, 10); // Pequeño retraso para activar la animación
+    }, 10); 
   }
 
-  // Desplazarse suavemente hacia la sección de las capas ISO/OSI
+  
   isoLayers.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
 
-// Acordeón para las capas
+
 document.querySelectorAll('.layer-header').forEach(header => {
   header.addEventListener('click', () => {
     const layerItem = header.parentElement;
